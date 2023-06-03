666758655cb73032806ff42d07afefc84a99ed54
scheng0718 <scheng0718@gmail.com>
2023-06-03 00:06:43 -0700

    feat: add edit function

--- app.js
+++ app.js
@@ -70,6 +70,44 @@ app.get('/restaurant/:id', (req, res) => {
     .then((restaurant) => res.render('show', {restaurant}))
     .catch(error => console.log(error))  
 })
+app.get('/restaurant/:id/edit', (req, res) => {
+  // console.log('req.params.id', req.params.id)
+  // 使用到解構賦值的寫法，從 req.params 中提取名為 id的屬性再賦值
+  const {id} = req.params
+  // const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === id)
+  // res.render('show', {restaurant})
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', {restaurant}))
+    .catch(error => console.log(error))  
+})
+app.post('/restaurant/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en  
+  const category = req.body.category  
+  const image = req.body.image  
+  const location = req.body.location  
+  const phone = req.body.phone  
+  const google_map = req.body.google_map  
+  const rating = req.body.rating  
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurant/${id}`))
+    .catch(error => console.log(error)) 
+})
 // Set up /search route
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
