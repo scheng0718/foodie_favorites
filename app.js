6164b4fb0ca7ea5a964dfa62b08f39c92b34e376
scheng0718 <scheng0718@gmail.com>
2023-06-05 12:01:33 -0700

    feat: update restaurantSeeder script and title for new.hbs page

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 // Load express modules
 const express = require('express')
-// Create an instance to use express
-const app = express()
-// Define server-related variables
-const port = 3000
+// Load express-handlebars
+const exphbs = require('express-handlebars')
 // Load mongoose
 const mongoose = require('mongoose')
-
+// Load Restaurant model
+const Restaurant = require('./models/restaurant')
+// Load JSON file
+// const restaurantList = require('./restaurant.json')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// Load express-handlebars
-const exphbs = require('express-handlebars')
-// Load JSON file
-const restaurantList = require('./restaurant.json')
-// Load Restaurant model
-const Restaurant = require('./models/restaurant')
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+
 // Set up db
 const db = mongoose.connection
 
@@ -27,6 +26,10 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+// Create an instance to use express
+const app = express()
+// Define server-related variables
+const port = 3000
 // Use template engine
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 // Set view engine as handlebars
@@ -35,30 +38,46 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 // Set up the root route and render through template engine 
+// 瀏覽全部餐廳
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurant => res.render('index', {restaurant}))
     .catch(error => console.error(error))
 })
+// 搜尋特定餐廳
+app.get('/search', (req, res) => {
+  if (!req.query.keyword) {
+    return res.redirect('/')
+  }
+  // console.log('req.query.keyword', req.query.keyword)
+  // Use trim() and regex to remove the spaces.
+  const keyword = req.query.keyword.trim().replace(/\s/g, '')
+  Restaurant.find({
+    // $or: [
+    //   { name: { $regex: keyword, $options: 'i' } }, // 使用正則表達式進行模糊搜尋，不區分大小寫
+    //   { category: { $regex: keyword, $options: 'i' } }
+    // ]
+  })
+    .lean()
+    .then(restaurant => {
+      const filteredRestaurant = restaurant.filter(data => data.name.toLowerCase().includes(keyword.toLowerCase()) || data.category.toLowerCase().includes(keyword.toLowerCase()))
+
+      res.render('index', {restaurant: filteredRestaurant, keyword})
+    })
+    .catch(error => console.log(error))
+})
+// 新增餐廳頁面
 app.get('/restaurant/new', (req, res) => {
   return res.render('new')
 })
 app.post('/restaurant/new', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en  
-  const category = req.body.category  
-  const image = req.body.image  
-  const location = req.body.location  
-  const phone = req.body.phone  
-  const google_map = req.body.google_map  
-  const rating = req.body.rating  
-  const description = req.body.description  
-  return Restaurant.create({name, name_en, category, image, location, phone, google_map, rating, description})
+  // create() 方法中直接傳入 req.body
+  return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-// Set up /restaurants/params and response body
+// 瀏覽特定餐廳
 app.get('/restaurant/:id', (req, res) => {
   // console.log('req.params.id', req.params.id)
   // 使用到解構賦值的寫法，從 req.params 中提取名為 id的屬性再賦值
@@ -70,6 +89,7 @@ app.get('/restaurant/:id', (req, res) => {
     .then((restaurant) => res.render('show', {restaurant}))
     .catch(error => console.log(error))  
 })
+// 編輯餐廳頁面
 app.get('/restaurant/:id/edit', (req, res) => {
   // console.log('req.params.id', req.params.id)
   // 使用到解構賦值的寫法，從 req.params 中提取名為 id的屬性再賦值
@@ -81,59 +101,21 @@ app.get('/restaurant/:id/edit', (req, res) => {
     .then((restaurant) => res.render('edit', {restaurant}))
     .catch(error => console.log(error))  
 })
+// 更新餐廳
 app.post('/restaurant/:id/edit', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.name_en  
-  const category = req.body.category  
-  const image = req.body.image  
-  const location = req.body.location  
-  const phone = req.body.phone  
-  const google_map = req.body.google_map  
-  const rating = req.body.rating  
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
+  // 在findByIdAndUpdate 直接傳進 id 和 req.body 會直接更新儲存
+  const {id} = req.params
+  return Restaurant.findByIdAndUpdate(id, req.body)
     .then(() => res.redirect(`/restaurant/${id}`))
     .catch(error => console.log(error)) 
 })
+// 刪除餐廳 直接使用 findByIdAndDelete()
 app.post('/restaurant/:id/delete', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
+  const {id} = req.params
+  return Restaurant.findByIdAndDelete(id)
+    // .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-})
-// Set up /search route
-app.get('/search', (req, res) => {
-  if (!req.query.keyword) {
-    return res.redirect('/')
-  }
-  // console.log('req.query.keyword', req.query.keyword)
-  // Use trim() and regex to remove the spaces.
-  const keyword = req.query.keyword.trim().replace(/\s/g, '')
-  Restaurant.find({
-    $or: [
-      { name: { $regex: keyword, $options: 'i' } }, // 使用正則表達式進行模糊搜尋，不區分大小寫
-      { category: { $regex: keyword, $options: 'i' } }
-    ]
-  })
-    .lean()
-    .then(restaurant => res.render('index', {restaurant, keyword}))
-    .catch(error => console.log(error))
-  // const restaurant = Restaurant.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-  // res.render('index', {restaurant, keyword})
 })
 
 // The server is listening and running at the http://localhost:3000
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,12 +2,17 @@
 const mongoose = require('mongoose')
 // Load Restaurant
 const Restaurant = require('../restaurant')
-// Load JSON file
-const restaurantList = require('/Users/sycheng/Code/foodie_favorites/restaurant.json')
+// Load JSON file 用 require(../../json file) 將檔案載入
+const restaurantList = require('../../restaurant.json').results
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.MONGODB_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
 
 // Set up db
 const db = mongoose.connection
@@ -17,21 +22,26 @@ db.on('error', () => {
 })
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  const restaurants = restaurantList.results
-  for (let restaurant of restaurants) {
-    Restaurant.create({
-      id: restaurant.id,
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description,
-    })  
-  }
-  console.log('mongodb done')
+  console.log('Running restaurantSeeder script!')
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log('restaurantSeeder done!')
+      db.close()
+    })
+    .catch(error => console.log(error))  
+  // const restaurants = restaurantList.results
+  // for (let restaurant of restaurants) {
+  //   Restaurant.create({
+  //     id: restaurant.id,
+  //     name: restaurant.name,
+  //     name_en: restaurant.name_en,
+  //     category: restaurant.category,
+  //     image: restaurant.image,
+  //     location: restaurant.location,
+  //     phone: restaurant.phone,
+  //     google_map: restaurant.google_map,
+  //     rating: restaurant.rating,
+  //     description: restaurant.description,
+  //   })  
+  // }
 })
