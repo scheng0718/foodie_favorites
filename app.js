d234470cf54042bef77cd00eedfc434fcbbec2e7
scheng0718 <scheng0718@gmail.com>
2023-06-08 00:44:20 -0700

    feat: implement RESTful style and add method-override package

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 // Load mongoose
 const mongoose = require('mongoose')
+// Load method override
+const methodOverride = require('method-override')
 // Load Restaurant model
 const Restaurant = require('./models/restaurant')
 // Load JSON file
@@ -37,6 +39,7 @@ app.set('view engine', 'hbs')
 // Use static file
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 // Set up the root route and render through template engine 
 // 瀏覽全部餐廳
 app.get('/', (req, res) => {
@@ -45,6 +48,30 @@ app.get('/', (req, res) => {
     .then(restaurant => res.render('index', {restaurant}))
     .catch(error => console.error(error))
 })
+// app.get('/filter', (req, res) => {
+//   const sortingOption = req.query.sorting;
+//   let sortQuery = {};
+
+//   if (sortingOption === 'asc') {
+//     sortQuery = { name_en: 1 };
+//   } else if (sortingOption === 'desc') {
+//     sortQuery = { name_en: -1 };
+//   } else if (sortingOption === 'category') {
+//     sortQuery = { category: 1 };
+//   } else if (sortingOption === 'location') {
+//     sortQuery = { location: 1 };
+//   }
+
+//   Restaurant.find()
+//     .lean()
+//     .sort(sortQuery)
+//     .then(restaurant => {
+//       res.render('index', { restaurant });
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// })
 // 搜尋特定餐廳
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
@@ -61,24 +88,24 @@ app.get('/search', (req, res) => {
   })
     .lean()
     .then(restaurant => {
-      const filteredRestaurant = restaurant.filter(data => data.name.toLowerCase().includes(keyword.toLowerCase()) || data.category.toLowerCase().includes(keyword.toLowerCase()))
+      const filteredRestaurant = restaurant.filter(data => data.name.toLowerCase().includes(keyword.toLowerCase()) || data.name_en.toLowerCase().includes(keyword.toLowerCase()) || data.category.toLowerCase().includes(keyword.toLowerCase()))
 
       res.render('index', {restaurant: filteredRestaurant, keyword})
     })
     .catch(error => console.log(error))
 })
 // 新增餐廳頁面
-app.get('/restaurant/new', (req, res) => {
+app.get('/restaurants/new', (req, res) => {
   return res.render('new')
 })
-app.post('/restaurant/new', (req, res) => {
+app.post('/restaurants', (req, res) => {
   // create() 方法中直接傳入 req.body
   return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 // 瀏覽特定餐廳
-app.get('/restaurant/:id', (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   // console.log('req.params.id', req.params.id)
   // 使用到解構賦值的寫法，從 req.params 中提取名為 id的屬性再賦值
   const {id} = req.params
@@ -90,7 +117,7 @@ app.get('/restaurant/:id', (req, res) => {
     .catch(error => console.log(error))  
 })
 // 編輯餐廳頁面
-app.get('/restaurant/:id/edit', (req, res) => {
+app.get('/restaurants/:id/edit', (req, res) => {
   // console.log('req.params.id', req.params.id)
   // 使用到解構賦值的寫法，從 req.params 中提取名為 id的屬性再賦值
   const {id} = req.params
@@ -102,15 +129,15 @@ app.get('/restaurant/:id/edit', (req, res) => {
     .catch(error => console.log(error))  
 })
 // 更新餐廳
-app.post('/restaurant/:id/edit', (req, res) => {
+app.put('/restaurants/:id', (req, res) => {
   // 在findByIdAndUpdate 直接傳進 id 和 req.body 會直接更新儲存
   const {id} = req.params
   return Restaurant.findByIdAndUpdate(id, req.body)
-    .then(() => res.redirect(`/restaurant/${id}`))
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error)) 
 })
 // 刪除餐廳 直接使用 findByIdAndDelete()
-app.post('/restaurant/:id/delete', (req, res) => {
+app.delete('/restaurants/:id', (req, res) => {
   const {id} = req.params
   return Restaurant.findByIdAndDelete(id)
     // .then(restaurant => restaurant.remove())
