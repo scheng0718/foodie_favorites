43faebacf53eb1266f2a89a6a1f384b22b86f802
scheng0718 <scheng0718@gmail.com>
2023-07-11 10:05:26 -0700

    feat: add user router in modules

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 // 將網址結構符合字串導向模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router
