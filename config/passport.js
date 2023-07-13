83a18afad2d552a31875eaf63cc0b372e1f9c0ab
scheng0718 <scheng0718@gmail.com>
2023-07-12 23:11:52 -0700

    feat: add github OAuth2 login

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const FacebookStrategy = require('passport-facebook').Strategy
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GitHubStrategy = require('passport-github2').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 // Local Strategy
@@ -79,6 +80,32 @@ passport.use(new GoogleStrategy({
       })
   }
 ))
+// Github Strategy
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK
+  },(accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(error => done(error, false))
+      })
+  }))
+
 passport.serializeUser((user, done) => {
   done(null, user.id)
 })
