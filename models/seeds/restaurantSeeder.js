110dfe6ca2bc68cb6ca3c35c3d9fe04ebbe498b0
scheng0718 <scheng0718@gmail.com>
2023-07-13 11:59:42 -0700

    refactor: modify user name attribute to not required and adjust error message. Refactor restaurant seeder data and add two seeder user

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -6,34 +6,48 @@ if (process.env.NODE_ENV !== 'production') {
 const Restaurant = require('../restaurant')
 const User = require('../user')
 // Load JSON file 用 require(../../json file) 將檔案載入
-const restaurantList = require('../../restaurant.json').results
+const restaurantList = require('./restaurant.json').results
 const db = require('../../config/mongoose')
-const SEED_USER = {
-  name: 'Admin',
-  email: 'admin@example.com',
+const SEED_USER = [{
+  name: 'user1',
+  email: 'user1@example.com',
   password: '12345678'
-}
-
-db.once('open', () => {
-  console.log('Running Restaurant Seeder script!')
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hash  
-    }))
-    .then(user => {
-      const userId = user._id
-      return Promise.all(restaurantList.map(restaurant => {
-        restaurant.userId = userId
-        return Restaurant.create(restaurant)
+},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}]
+// 用 async 關鍵字表示非同步函式
+db.once('open', async () => {
+  try {
+    console.log('Running Restaurant Seeder script!')
+    for (let i = 0; i < SEED_USER.length; i++) {
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(SEED_USER[i].password, salt)
+      const user = await User.create({
+        name: SEED_USER[i].name,
+        email: SEED_USER[i].email,
+        password: hash
+      })
+      // 在現有 restaurant 資料中加入新的屬性 userId，回傳新的物件 
+      const restaurants = restaurantList.slice(i * 3, (i + 1) * 3).map(restaurant => ({
+        ...restaurant,
+        userId: user._id
       }))
-    })
-    .then(() => {
-      console.log('Restaurant Seeder done!')
-      process.exit()
-    })
-    .catch(error => console.log(error))  
+      // 用 await 關鍵字等首三筆資料插入到資料庫之後，再進行下一次迴圈
+      await Restaurant.insertMany(restaurants)
+    }
+    console.log('Restaurant Seeder done!')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 })
+
+
+
+
+
+
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -18,8 +18,8 @@ router.get('/signup', (req, res) => {
 router.post('/signup', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: 'All fields are required.'})
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'Email and Password are required.'})
   }
   if (password !== confirmPassword) {
     errors.push({ message: 'Password and Confirm Password are not matched.'})
