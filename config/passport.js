8dbd3d2de293c26f5508e66fae91713ad50b43b3
scheng0718 <scheng0718@gmail.com>
2023-07-12 18:31:00 -0700

    feat: add google login via passport strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const FacebookStrategy = require('passport-facebook')
+const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 // Local Strategy
@@ -51,7 +52,33 @@ passport.use(new FacebookStrategy({
           .then(user => done(null, user))
           .catch(error => done(error, false))
       })
-  }))
+}))
+// Google Strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },(accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(error => done(error, false))
+      })
+  }
+))
 passport.serializeUser((user, done) => {
   done(null, user.id)
 })
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -2,14 +2,22 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
-router.get('/facebook',
-  passport.authenticate('facebook', { 
+router.get('/facebook', passport.authenticate('facebook', { 
     scope: ['email', 'public_profile'] 
-}));
+}))
 
 router.get('/facebook/callback', passport.authenticate('facebook', { 
-    successRedirect: '/',
-    failureRedirect: '/login' 
+  successRedirect: '/',
+  failureRedirect: '/login' 
+}))
+
+router.get('/google', passport.authenticate('google', { 
+    scope: ['email', 'profile'] 
+}))
+
+router.get('/google/callback', passport.authenticate('google', { 
+  successRedirect: '/',
+  failureRedirect: '/login' 
 }))
 
 module.exports = router
