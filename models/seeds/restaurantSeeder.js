17dcc06ddb52076fe2f91caa1497b2570569f2a8
scheng0718 <scheng0718@gmail.com>
2023-07-13 00:03:53 -0700

    feat: modify restaurant seeder data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,30 +1,39 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // Load Restaurant
 const Restaurant = require('../restaurant')
+const User = require('../user')
 // Load JSON file 用 require(../../json file) 將檔案載入
 const restaurantList = require('../../restaurant.json').results
-const db = require('../../config/mongoose'
-)
+const db = require('../../config/mongoose')
+const SEED_USER = {
+  name: 'Admin',
+  email: 'admin@example.com',
+  password: '12345678'
+}
+
 db.once('open', () => {
-  console.log('Running restaurantSeeder script!')
-  Restaurant.create(restaurantList)
+  console.log('Running Restaurant Seeder script!')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash  
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(restaurantList.map(restaurant => {
+        restaurant.userId = userId
+        return Restaurant.create(restaurant)
+      }))
+    })
     .then(() => {
-      console.log('restaurantSeeder done!')
-      db.close()
+      console.log('Restaurant Seeder done!')
+      process.exit()
     })
     .catch(error => console.log(error))  
-  // const restaurants = restaurantList.results
-  // for (let restaurant of restaurants) {
-  //   Restaurant.create({
-  //     id: restaurant.id,
-  //     name: restaurant.name,
-  //     name_en: restaurant.name_en,
-  //     category: restaurant.category,
-  //     image: restaurant.image,
-  //     location: restaurant.location,
-  //     phone: restaurant.phone,
-  //     google_map: restaurant.google_map,
-  //     rating: restaurant.rating,
-  //     description: restaurant.description,
-  //   })  
-  // }
 })
